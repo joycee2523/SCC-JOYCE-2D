f6f3de6f880354365bcb6e2f5e0a0eeda946229b
joycee2523 <camillegeocallo05@gmail.com>
2024-09-21 20:01:58 +0800

    my prelim repo

---
+++ src/Grade.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author geoca
+ */
+class Grade {
+
+    void getGradeInput() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void editGrades(int editId, double newPr) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void viewGrades() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void addGrades(String name, int id, double pr, double md, double pf, double fn) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
